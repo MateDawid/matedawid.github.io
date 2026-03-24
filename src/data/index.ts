59cd47ee48017bd3fb398e67eeddce5384bd4f58
MateDawid <mateusiakdawid@gmail.com>
2026-03-24 17:30:09 +0100

    EducationCard - prettier

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,7 +11,7 @@ import about_card_json from "./about_card.json";
 import skills_card_json from "./skills_card.json";
 import experience_card_json from "./experience_card.json";
 import projects_card_json from "./projects_card.json";
-import education_card_json from "./education_card.json"; 
+import education_card_json from "./education_card.json";
 import skill_chip_colors_json from "./skill_chip_colors.json";
 
 export const profile_card_data: ProfileCardData = profile_card_json;
